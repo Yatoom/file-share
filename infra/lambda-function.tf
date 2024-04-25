74af75424cf77e9e828ec9a5a42c205506f95399
Jeroen van Hoof <jeroen.van.hoof@rabobank.nl>
2024-04-25 08:32:19 +0200

    Disable terraform

--- infra/lambda-function.tf
+++ infra/lambda-function.tf
@@ -25,4 +25,5 @@ resource "aws_lambda_function" "lambda" {
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   runtime       = "provided.al2"
+  s3_bucket      = 
 }
